c73f5f0dde7a4ffa76206ff2e8fc2cef19a8a416
durak-online <dedkovewan417@yandex.com>
2026-04-21 18:47:06 +0500

    a

--- index.js
+++ index.js
@@ -127,3 +127,5 @@ form.addEventListener('input', event => {
   const preview = event.target.closest('.wishes-field').querySelector('.wishes-preview');
   preview.innerHTML = highlightUrgent(event.target.value);
 });
+
+
